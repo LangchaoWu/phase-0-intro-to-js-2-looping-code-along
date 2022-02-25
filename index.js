ed9305aa50542cb266171996f7baedd05d09b1cc
Langchao Wu <langchaowu@yahoo.com>
2022-02-25 10:50:02 -0500

    commit tree e96de15078ffbfed2de15c594781d4cf3f8baf77

--- index.js
+++ index.js
@@ -1,1 +1,14 @@
 // Code your solutions in this file
+function writeCards(name,event){
+    const arry=[]
+    for(let i=0;i<name.length;i++){
+        arry[i]=`Thank you, ${name[i]}, for the wonderful ${event} gift!`;
+    }
+    return arry;
+}
+
+function countDown(num){
+    for(let i=num;i>=0;i--){
+        console.log(i);
+    }
+}
